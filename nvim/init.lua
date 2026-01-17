4923e564c2c6382911fca79573e3d9e8dc9825fc
eternalbluephy <peihongyu123@126.com>
2026-01-17 20:57:27 +0800

    neovim: Use vim.lsp instead of nvim-lspconfig

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
+require("lsp")
 vim.cmd.colorscheme "catppuccin"
---
+++ nvim/lsp/clangd.lua
@@ -0,0 +1,21 @@
+return {
+	cmd = { "clangd" },
+	filetypes = { "cpp", "cuda", "c", "cc", "objc", "objcpp" },
+	root_markers = {
+    '.clangd',
+    '.clang-tidy',
+    '.clang-format',
+    'compile_commands.json',
+    'compile_flags.txt',
+    'configure.ac', -- AutoTools
+    '.git',
+  },
+	capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { 'utf-8', 'utf-16' },
+  },
+}
